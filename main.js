6351cd0c602d0f2128274b9e39b87559a1e41110
Fortunat Mutunda <fotimutunda@gmail.com>
2016-04-05 23:36:51 +0300

    one mother box

--- main.js
+++ main.js
@@ -1,15 +1,27 @@
-import React from 'react'
+import React,{createClass} from 'react'
 import ReactDOM,{render} from 'react-dom'
 import Randomwords from './Components/randomwords'
 import Stats from './Components/stats'
 import Input from './Components/userinput'
 
 
+var MainContainer = createClass({
+
+    render: function () {
+
+        return (<div>
+            <Randomwords />
+            <Input/>
+            <Stats/>
+        </div>);
+
+    }
+});
+
+
 render(
   <div>
-      <Randomwords />
-      <Input/>
-      <Stats/>
+      <MainContainer/>
   </div>,
   document.getElementById('content')
 );
